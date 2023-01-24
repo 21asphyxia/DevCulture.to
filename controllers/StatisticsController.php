b59b5ebd11f4cb37973d570d9b6b54da2c81536b
21asphyxia <mouadelamraoui@gmail.com>
2023-01-24 17:28:28 +0100

    [CT-20] Statistics done

--- controllers/StatisticsController.php
+++ controllers/StatisticsController.php
@@ -2,4 +2,11 @@
 include_once __DIR__.'/../models/Crud.class.php';
 
 
-$users = new CRUD;
+$stats = new CRUD;
+
+$totalArticles = $stats->read('articles', 'COUNT(*) as total')[0]['total'];
+
+$totalCategories = $stats->read('categories', 'COUNT(*) as total')[0]['total'];
+
+// get users with number of articles
+$allDevs = $stats->read('admins', 'admins.id, admins.username, COUNT(articles.id) as total', ['articles'], ['admins.id = articles.admin_id'], null, 'total DESC', 5);
